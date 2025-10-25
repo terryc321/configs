3c544f39469ae0e833dde6dfaa71d7115ca74fb8
terry <terry.cadd@gmail.com>
2025-10-25 21:08:14 +0100

    added a few params to emacs build script

--- emacs/build.sh
+++ emacs/build.sh
@@ -3,18 +3,21 @@
 set -e 
 
 ./configure \
-  --prefix=/opt/emacs
-  --without-compress-install \
-  --with-native-compilation \
-  --with-tree-sitter \
-  --with-json \
-  --with-mailutils \
-  --with-cairo \
-  --with-harfbuzz \
-  --with-imagemagick \
+    --prefix=/opt/emacs \
+    --with-wide-int \
+    --with-x-toolkit=gtk3 \
+    --without-xaw3d \
+    --with-libsystemd \
+    --without-compress-install \
+    --with-native-compilation \
+    --with-tree-sitter \
+    --with-json \
+    --with-mailutils \
+    --with-cairo \
+    --with-harfbuzz \
+    --with-imagemagick \
   CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
 
 make -j8
 sudo make install
-
 
