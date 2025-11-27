b4ff935bc51b8c104b0139b9fc6ac611aa95ecf2
terry <terry.cadd@gmail.com>
2025-11-27 08:35:00 +0000

    adding idris2 to the fold

---
+++ idris2/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+make bootstrap-racket
+
+# make install step
+IDRIS2_CG=racket make install
+
+# install idris2 library documentation
+make install-libdocs
+
+#"${BROWSER:-echo}" "$(idris2 --libdir)"/docs/index.html
+
+#run tests
+make test
+
+#make install-api
+
+# shell bash completion
+#eval "$(idris2 --bash-completion-script idris2)
+
